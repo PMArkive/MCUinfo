bbe9149bdb5bec223dda8b8d342dfbade95a0b86
Hikari-chan <yuukishiroko@gmail.com>
2017-01-30 11:19:45 -0500

    spacing

--- source/main.c
+++ source/main.c
@@ -8,30 +8,29 @@ u8 mcuFWHigh;
 u8 mcuFWLow;
 
 int main(int argc, char **argv) {
-    gfxInitDefault();
-    consoleInit(GFX_TOP, NULL);
-    printf("MCU info\nby Hikari\n\n");
-    printf("Press B to exit.\n");
-    printf("Initializing the MCU...\n");
-    mcuInit();
-    GetMcuFwVerHigh(&mcuFWHigh);
-    printf("McuFwVerHigh: %u\n", mcuFWHigh);
-    GetMcuFwVerLow(&mcuFWLow);
-    printf("McuFwVerLow: %u\n", mcuFWLow);
-      while (aptMainLoop()) {
-        hidScanInput();
-        u32 kDown = hidKeysDown();
-          if (kDown & KEY_B) {
-            printf("Exiting...\n");
-            break;
-          }
-        }
+	gfxInitDefault();
+	consoleInit(GFX_TOP, NULL);
+	printf("MCU info\nby Hikari\n\n");
+	printf("Press B to exit.\n");
+	printf("Initializing the MCU...\n");
+	mcuInit();
+	GetMcuFwVerHigh(&mcuFWHigh);
+	printf("McuFwVerHigh: %u\n", mcuFWHigh);
+	GetMcuFwVerLow(&mcuFWLow);
+	printf("McuFwVerLow: %u\n", mcuFWLow);
+	while (aptMainLoop()) {
+		hidScanInput();
+		u32 kDown = hidKeysDown();
+		if (kDown & KEY_B) {
+		printf("Exiting...\n");
+		break;
+	}
+}
 
-    mcuExit();
-    gfxFlushBuffers();
-    gfxSwapBuffers();
-    gspWaitForVBlank();
-
-    gfxExit();
-    return 0;
+	mcuExit();
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	gspWaitForVBlank();
+	gfxExit();
+	return 0;
 }
